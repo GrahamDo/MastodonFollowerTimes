df76875c0a5da0e5f21897c011e7fe643e643b0a
Graham Downs <graham@communicate.za.net>
2022-12-26 13:44:09 +0200

    Add databinding with viewmodel

---
+++ MastodonFollowerTimes/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Mime;
+
+namespace MastodonFollowerTimes
+{
+    internal class MainWindowViewModel
+    {
+        public string WindowTitle => "Mastodon Follower Times: Fine the best time to post";
+    }
+}
