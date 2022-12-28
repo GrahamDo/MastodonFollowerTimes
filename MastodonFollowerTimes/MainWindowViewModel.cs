d16d088e91fbf52054fdd4c1e15c5e249a27aead
Graham Downs <graham@communicate.za.net>
2022-12-28 13:06:17 +0200

    Disable controls while running, Update list values on property changed

--- MastodonFollowerTimes/MainWindowViewModel.cs
+++ MastodonFollowerTimes/MainWindowViewModel.cs
@@ -7,19 +7,30 @@ namespace MastodonFollowerTimes
         public string WindowTitle => "Mastodon Follower Times: Fine the best time to post";
         public WpfSettings Settings { get; }
         public ObservableCollection<StatusPerHour> StatusesPerHour { get; set; }
+        public bool EnableControls { get; set; }
 
         public MainWindowViewModel()
         {
             Settings = WpfSettings.Load();
             StatusesPerHour = new ObservableCollection<StatusPerHour>();
+            EnableControls = true;
         }
 
         public void LoadData()
         {
-            StatusesPerHour.Clear();
-            for (byte i = 0; i < 25; i++)
+            EnableControls = false;
+            try
             {
-                StatusesPerHour.Add(new StatusPerHour { Hour = i, StatusCount = 25 + (uint)i, TotalStatuses = 240 });
+                StatusesPerHour.Clear();
+                for (byte i = 0; i < 25; i++)
+                {
+                    StatusesPerHour.Add(new StatusPerHour
+                        { Hour = i, StatusCount = 25 + (uint)i, TotalStatuses = 240 });
+                }
+            }
+            finally
+            {
+                EnableControls = true;
             }
         }
     }
